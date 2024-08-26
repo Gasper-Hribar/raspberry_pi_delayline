210b4d5661df94e5eb019a5c2d55a73763598632
Gasper-Hribar <hribar.gasper@hotmail.com>
2024-08-26 10:12:01 +0200

    Initialized window size fix.

--- main_mini.py
+++ main_mini.py
@@ -1134,6 +1134,7 @@ class delayProgramator_app(tk.Tk):
         self.title('Delay Line Programator')
         # app window starts in borderless fullscreen mode
         self.attributes("-fullscreen", True)
+        self.geometry("800x480")
         self.bind("<F11>", lambda event: self.attributes(
             "-fullscreen", not self.attributes("-fullscreen")))
         # keyboard key bindings for exiting fullscreen mode
